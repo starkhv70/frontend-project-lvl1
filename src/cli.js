21950086585a6c1c02980e7d0bf4b8c69032357b
starkhv70 <starkhv@yandex.ru>
2020-08-15 23:00:30 +1000

    step 4

--- src/cli.js
+++ src/cli.js
@@ -1,6 +1,8 @@
 import readlineSync from 'readline-sync';
 
-export const greetingUser =() => {
-    const userName = readlineSync.question('May I have your name? ', {limit: /\p{L}/gu});
-    console.log(`Hello, ${userName}!`)
+const greetingUser = () => {
+  const userName = readlineSync.question('May I have your name? ', { limit: /\p{L}/gu });
+  console.log(`Hello, ${userName}!`);
 };
+
+export default greetingUser;
