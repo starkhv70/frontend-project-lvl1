b216022d9113bedd70104203bae777550451aaa9
BKM <starkhv@yandex.ru>
2020-08-17 17:58:01 +1000

    add util function

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -5,3 +5,9 @@ console.log('Welcome to the Brain Games!');
 const userName = getUserName();
 console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
+const numOfTries = 3;
+const maxNum = 200;
+for (let i = 0; i < numOfTries; i += 1) {
+  const randomNum = generateNumber(maxNum);
+  console.log(`Question: ${randomNum}`);
+}
--- src/util.js
+++ src/util.js
@@ -11,4 +11,15 @@ const generateNumber = (max) => Math.floor(Math.random() * Math.floor(max));
 
 const isEven = (num) => num % 2 === 0;
 
-export { getUserName, generateNumber, isEven };
+const getYNAnswer = (query) => {
+  const answer = readlineSync.question(query,
+    { trueValue: ['yes'], falseValue: ['no'] });
+  return (answer === true) ? 'yes' : 'no';
+};
+
+export {
+  getUserName,
+  generateNumber,
+  isEven,
+  getYNAnswer,
+};
