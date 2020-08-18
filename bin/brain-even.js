133ebc1e34bbd0b809e8aae4fc9468c3de068400
BKM <starkhv@yandex.ru>
2020-08-18 14:58:27 +1000

    refactoring brain-even

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,30 +1,14 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
-import { generateNumber, isEven, convertYNAnswer } from '../src/util.js';
-
-const numOfTries = 3;
-const maxNum = 200;
+import { greetingUser, playGame } from '../src/util.js';
 
 console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name? ', { limit: /\p{L}/gu });
-console.log(`Hello, ${userName}!`);
+const userName = greetingUser();
 console.log('Answer "yes" if the number is even, otherwise answer "no".');
 
-let isSuccess = true;
-for (let i = 0; i < numOfTries; i += 1) {
-  const randomNum = generateNumber(maxNum);
-  console.log(`Question: ${randomNum}`);
-  const answer = readlineSync.question('Your answer:',
-    { trueValue: ['yes'], falseValue: ['no'] });
-  if (isEven(randomNum) !== answer) {
-    isSuccess = false;
-    console.log(`"${convertYNAnswer(answer)}" is wrong answer ;(. Correct answer was "${isEven(randomNum) ? 'yes' : 'no'}".`);
-    console.log(`Let's try again, ${userName}!`);
-    break;
-  }
-  console.log('Correct!');
-}
+const isSuccess = playGame();
 
 if (isSuccess) {
   console.log(`Congratulations, ${userName}!`);
+} else {
+  console.log(`Let's try again, ${userName}!`);
 }
--- src/util.js
+++ src/util.js
@@ -1,3 +1,11 @@
+import readlineSync from 'readline-sync';
+
+const greetingUser = () => {
+  const userName = readlineSync.question('May I have your name? ', { limit: /\p{L}/gu });
+  console.log(`Hello, ${userName}!`);
+  return userName;
+};
+
 const generateNumber = (max) => Math.floor(Math.random() * Math.floor(max));
 
 const isEven = (num) => num % 2 === 0;
@@ -10,10 +18,22 @@ const convertYNAnswer = (answer) => {
   }
   return answer;
 };
+const playGame = (numOfTries = 3, maxNum = 200) => {
+  for (let i = 0; i < numOfTries; i += 1) {
+    const randomNum = generateNumber(maxNum);
+    console.log(`Question: ${randomNum}`);
+    const answer = readlineSync.question('Your answer:',
+      { trueValue: ['yes'], falseValue: ['no'] });
+    if (isEven(randomNum) !== answer) {
+      console.log(`"${convertYNAnswer(answer)}" is wrong answer ;(. Correct answer was "${isEven(randomNum) ? 'yes' : 'no'}".`);
+      return false;
+    }
+    console.log('Correct!');
+  }
+  return true;
+};
 
 export {
-  generateNumber,
-  isEven,
-  isBoolean,
-  convertYNAnswer,
+  greetingUser,
+  playGame,
 };
