19a2a2ecfb9fed72dacac5d54f641685063a6e7e
starkhv70 <starkhv@yandex.ru>
2020-09-01 23:18:37 +1000

    correct mistake in function generateRandomNumber

--- src/util.js
+++ src/util.js
@@ -1,4 +1,7 @@
 /* eslint-disable import/prefer-default-export */
-const generateRandomNumber = (min, max) => (min + Math.floor(Math.random() * Math.floor(max)));
+const generateRandomNumber = (min, max) => {
+  const randomNumber = min + Math.floor(Math.random() * Math.floor(max - min));
+  return randomNumber;
+};
 
 export { generateRandomNumber };
